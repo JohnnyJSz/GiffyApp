4f26e8c1b3287f01a268c3dae78c958a0ad3540d
Jonathan <jonathan.salazar@vieweet.com>
2022-01-03 22:42:54 +0000

    Added SearchForm component

---
+++ src/components/searchForm/SearchForm.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { useLocation } from "wouter";
+import Search from "../../images/search/search.svg";
+import classes from "./SearchForm.module.css";
+
+const SearchForm = () => {
+  const [keyword, setKeyword] = useState("");
+  const [, toLocation] = useLocation();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    toLocation(`/search/${keyword}`);
+  };
+
+  const inputChangeHandler = (event) => {
+    setKeyword(event.target.value);
+  };
+
+  return (
+    <form onSubmit={submitHandler} className={classes.form}>
+      <input
+        type="text"
+        name="search"
+        id="search-input"
+        value={keyword}
+        onChange={inputChangeHandler}
+        className={classes.formInput}
+        placeholder="Search..."
+      />
+      <button className={classes.button}>
+        <img src={Search} alt="Search" />
+      </button>
+    </form>
+  );
+};
+
+export default SearchForm;
