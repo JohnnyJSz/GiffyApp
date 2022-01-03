e0db646293525f6448473d679d97451443b62ceb
Jonathan <jonathan.salazar@vieweet.com>
2022-01-03 22:34:43 +0000

    Some cleanup made and added some style changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Link } from "wouter";
+import Home from "./pages/home";
+import Detail from "./pages/details";
+import SearchResults from "./pages/searchResults";
+import { HOME, DETAILS, SEARCH_RESULTS } from "./config/paths";
+
+import classes from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.App}>
+      <Link to={HOME}>
+        <div className={classes.AppTitle}>
+          <h1 className={classes.AppTitleText}>Giffy</h1>
+        </div>
+      </Link>
+      <Route path={HOME} component={Home} />
+      <Route path={SEARCH_RESULTS} component={SearchResults} />
+      <Route path={DETAILS} component={Detail} />
     </div>
   );
 }
